add2cc2a749f86f9d1f48e323d569336f8330a1c
ZakCrissinger <zcris001@plattsburgh.edu>
2020-03-24 17:18:14 -0400

    Integration

--- app/src/main/java/com/example/plattsmapnavigation/LogInActivity.java
+++ app/src/main/java/com/example/plattsmapnavigation/LogInActivity.java
@@ -1,14 +1,102 @@
 package com.example.plattsmapnavigation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+//sign in activity
 public class LogInActivity extends AppCompatActivity {
-
+    EditText emailID, password;
+    Button btnSignIn;
+    TextView tvSignUp;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        mFirebaseAuth = FirebaseAuth.getInstance(); //create firebase instance
+        emailID = findViewById(R.id.editText);  //get email from textbox in SignUpActivity.xml
+        password = findViewById(R.id.editText2); //get password from textbox in SignUpActivity.xml
+        btnSignIn = findViewById(R.id.button);
+        tvSignUp = findViewById(R.id.textView);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if(mFirebaseUser != null){
+                    Toast.makeText(LogInActivity.this, "You are logged in", Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(LogInActivity.this, MapsActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(LogInActivity.this, "Please Login", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        btnSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String email = emailID.getText().toString();
+                String pwd = password.getText().toString();
+                if (email.isEmpty()) {
+                    emailID.setError("Provide your email");
+                    emailID.requestFocus();
+                }
+                else if (pwd.isEmpty()) {
+                    password.setError("Provide your password");
+                    password.requestFocus();
+                }
+                else if (email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(LogInActivity.this, "Fields are both empty!",Toast.LENGTH_SHORT).show();
+                }
+                else if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LogInActivity.this, "Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Intent inToHome = new Intent(LogInActivity.this, MapsActivity.class);
+                                startActivity(inToHome);
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(LogInActivity.this, "ERROR OCCURRED",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intSignUp = new Intent(LogInActivity.this, SignUpActivity.class);
+                startActivity(intSignUp);
+            }
+        });
+    }
+
+    protected void onStart(){
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 }
--- app/src/main/java/com/example/plattsmapnavigation/SignUpActivity.java
+++ app/src/main/java/com/example/plattsmapnavigation/SignUpActivity.java
@@ -1,14 +1,79 @@
 package com.example.plattsmapnavigation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.plattsmapnavigation.LogInActivity;
+import com.example.plattsmapnavigation.MapsActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+//SignUpActivity activity
 public class SignUpActivity extends AppCompatActivity {
-
+    EditText emailID, password;
+    Button btnSignUp;
+    TextView tvSignIn;
+    FirebaseAuth mFirebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        mFirebaseAuth = FirebaseAuth.getInstance(); //create firebase instance
+        emailID = findViewById(R.id.editText);  //get email from textbox in SignUpActivity.xml
+        password = findViewById(R.id.editText2); //get password from textbox in SignUpActivity.xml
+        btnSignUp = findViewById(R.id.button);
+        tvSignIn = findViewById(R.id.textView);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailID.getText().toString();
+                String pwd = password.getText().toString();
+                if (email.isEmpty()) {
+                    emailID.setError("Provide your email");
+                    emailID.requestFocus();
+                }
+                else if (pwd.isEmpty()) {
+                    password.setError("Provide your password");
+                    password.requestFocus();
+                }
+                else if (email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(SignUpActivity.this, "Fields are both empty!",Toast.LENGTH_SHORT).show();
+                }
+                else if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email,pwd).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(SignUpActivity.this, "SignUpActivity Unsuccessful, Please Try Again.",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                startActivity(new Intent(SignUpActivity.this, MapsActivity.class));
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(SignUpActivity.this, "ERROR OCCURRED",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        tvSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(SignUpActivity.this, LogInActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
