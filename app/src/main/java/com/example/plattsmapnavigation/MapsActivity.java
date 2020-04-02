18e52baa9c1af2c3fd9611195bf2b9f791772048
Amart076 <Amart076@plattsburgh.edu>
2020-04-01 22:09:26 -0400

    Restructured maps layout to include functional search bar, and restricted map bounds to include campus only

--- app/src/main/java/com/example/plattsmapnavigation/MapsActivity.java
+++ app/src/main/java/com/example/plattsmapnavigation/MapsActivity.java
@@ -1,8 +1,11 @@
 package com.example.plattsmapnavigation;
 
 import androidx.fragment.app.FragmentActivity;
-
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +13,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -35,13 +42,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    public void onMapSearch(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng plattsburgh   = new LatLng(44.6930116, -73.4653251);
-        mMap.addMarker(new MarkerOptions().position(plattsburgh).title("Plattsburgh"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(plattsburgh));
+
+        LatLng one = new LatLng(44.6950, -73.4669);
+        LatLng two = new LatLng(44.6920, -73.463);
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+        builder.include(one);
+        builder.include(two);
+
+        LatLngBounds bounds = builder.build();
+
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+
+        int padding = (int) (width * 0.20);
+        mMap.setLatLngBoundsForCameraTarget(bounds);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding));
+        mMap.setMinZoomPreference(mMap.getCameraPosition().zoom);
     }
 }
