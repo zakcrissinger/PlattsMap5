0d66e05aa5781e24a4daa41be1972b831356a0b8
noah-lambert <nl111799@gmail.com>
2020-04-09 15:35:36 -0400

    fixed typo in MapsActivity

--- app/src/main/java/com/example/plattsmapnavigation/MapsActivity.java
+++ app/src/main/java/com/example/plattsmapnavigation/MapsActivity.java
@@ -92,9 +92,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
-    /*
-     */
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
