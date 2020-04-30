6f9fa4691ea474a458be0f434f1061bd6347b841
zakcrissinger <42742686+zakcrissinger@users.noreply.github.com>
2020-04-29 22:40:04 -0400

    Home screen that welcomes user, tells to sign in, displays next class if applicable

--- app/src/main/java/com/example/plattsmapnavigation/MainActivity.java
+++ app/src/main/java/com/example/plattsmapnavigation/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,20 +19,31 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.view.Menu;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-
+    public static FirebaseFirestore db = FirebaseFirestore.getInstance();
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     NavigationView navigationView;
     ActionBarDrawerToggle toggle;
     Menu menu;
-    TextView welcomeMessage;
+    public TextView welcomeMessage, nextClass;
+    List<List<String>> allClasses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +55,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
         menu = navigationView.getMenu();
         welcomeMessage = findViewById(R.id.welcome);
+        nextClass = findViewById(R.id.nextclass);
         AdjustSignInState(menu);
         setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
         toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawerOpen,R.string.drawerClose);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-        fixGoogleMapBug();
+        //fixGoogleMapBug();
         welcomeMessage.setText(updateWelcomeMessage());
+        if(!SignInStatus.SignedIn){
+            nextClass.setText("Sign in to see your next class!");
+        }
+        else{
+            getClasses();
+        }
     }
 
     private void fixGoogleMapBug() {
@@ -112,7 +131,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return false;
     }
 
-    public String getTimeOfDay(){
+    private String getTimeOfDay(){
+        int hour = hourAndMinutes()[0];
+        if(hour < 12){
+            return "Good Morning, ";
+        }
+        else if(hour >= 12 & hour < 17){
+            return "Good Afternoon, ";
+        }
+        else{
+            return "Good Evening, ";
+        }
+    }
+    private int[] hourAndMinutes(){
         String time = Calendar.getInstance().getTime().toString();
         int i = 0;
         char[] now = new char[8];
@@ -130,38 +161,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         int hour = Character.getNumericValue(now[0])*10;
         hour = hour + Character.getNumericValue(now[1]);
-        if(hour < 12){
-            return "Good Morning, ";
-        }
-        else if(hour >= 12 & hour < 17){
-            return "Good Afternoon, ";
-        }
-        else{
-            return "Good Evening, ";
-        }
+        int minute = Character.getNumericValue(now[3])*10;
+        minute = minute + Character.getNumericValue(now[4]);
+        int[] times = new int[2];
+        times[0] = hour;
+        times[1] = minute;
+        return(times);
     }
-    public String getDayOfWeek(){
+    private String getDayOfWeek(){
         Calendar calendar = Calendar.getInstance();
         int day = calendar.get(Calendar.DAY_OF_WEEK);
         switch(day){
             case 1:
-                return("Sunday");
+                return("Sun");
             case 2:
-                return("Monday");
+                return("M");
             case 3:
-                return("Tuesday");
+                return("T");
             case 4:
-                return("Wednesday");
+                return("W");
             case 5:
-                return("Thursday");
+                return("Th");
             case 6:
-                return("Firday");
+                return("F");
             case 7:
-                return("Saturday");
+                return("Sat");
         }
     return null;
     }
-    public String updateWelcomeMessage(){
+    private String updateWelcomeMessage(){
         if(SignInStatus.SignedIn){
             String str = SignInStatus.UserName.split("@")[0];
             String name = str.substring(0, 1).toUpperCase() + str.substring(1);
@@ -171,4 +199,82 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             return("Welcome to PlattsMap!");
         }
     }
-}
+    private void sendClasses(List<List<String>> list){
+        //nextClass.setText(updateNextClassMessage());
+        int[] times = hourAndMinutes();
+        char day = getDayOfWeek().toCharArray()[0];
+        int timeGap = 100000000;
+        int i = 0, theClass = 0;
+        for(List<String> cls: list){
+            int j = 0;
+            while(j < cls.get(2).length()){
+                //if(day == 'T'){
+                //
+               // }
+                if(day == cls.get(2).charAt(j)){
+                    System.out.println("day is " + day);
+                    System.out.println("class day is" + cls.get(2).charAt(j) );
+                    int classHour;
+                    if(cls.get(3).length() == 4){
+                        classHour = Character.getNumericValue(cls.get(3).charAt(0));
+                    }
+                    else{
+                        classHour = Character.getNumericValue(cls.get(3).charAt(0))*10;
+                        classHour = classHour + Character.getNumericValue(cls.get(3).charAt(1));
+                    }
+                    int classMin = Character.getNumericValue(cls.get(3).charAt(0))*10;
+                    classMin = classMin + Character.getNumericValue(cls.get(3).charAt(1));
+                    int now = times[0] + times[1]/60;
+                    int classTime = classHour + classMin/60;
+                    if(classTime - now > 0 & classTime - now < timeGap){
+                        timeGap = classTime - now;
+                        theClass = i;
+                    }
+                }
+                j++;
+            }
+            i++;
+        }
+        if(timeGap == 100000000){
+            nextClass.setText("No more classes for the day!");
+        }
+        else{
+            List<String> closeClass = list.get(theClass);
+            String message = "Your next class is " + closeClass.get(0) + " in " + closeClass.get(1) + " Hall at " + closeClass.get(3);
+            nextClass.setText(message);
+        }
+
+
+    }
+
+
+    private String getClasses() {
+        Query qClasses;
+        List<List<String>> classes = new ArrayList<>();
+        try {
+            qClasses = db.collection("users").document(SignInStatus.UserName).collection("classes");
+        }
+        catch (Exception NullPointerException){
+            return("Enter a schedule to see your upcoming classes!");
+        }
+
+        qClasses.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            List<String> list = new ArrayList<>();
+                            list.add(document.getString("class"));
+                            list.add(document.getString("location"));
+                            list.add(document.getString("start"));
+                            list.add(document.getString("end"));
+                            list.add(document.getString("days"));
+                            classes.add(list);
+                        }
+                    }
+                    sendClasses(classes);
+                }
+            });
+        return null;
+        }
+    }
